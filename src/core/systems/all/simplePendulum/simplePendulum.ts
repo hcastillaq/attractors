cb89a0db0ecbacc40d701dd54072a6ee8d4a283d
hernan <hcastillaq@gmail.com>
2021-11-22 14:08:45 -0500

    change angles in z and inital angles

--- src/core/systems/all/simplePendulum/simplePendulum.ts
+++ src/core/systems/all/simplePendulum/simplePendulum.ts
@@ -2,15 +2,17 @@ import ParticleSystem from "../../config/system";
 import { SystemAndConfig } from "../../systems";
 
 class SimplePendulum extends ParticleSystem {
+  private l = [50, 100, 150, 200];
   makeParticle() {
     return {
       x: 0,
       y: 0,
       z: 0,
-      l: this.random(0.1, 2),
+      l: this.l[Math.floor(Math.random() * this.l.length)],
       angle: this.random(-Math.PI / 2, Math.PI / 2),
+      angle2: this.random(-Math.PI, Math.PI),
       angleV: 0,
-      g: 0.0001,
+      g: 0.005,
     };
   }
 
@@ -18,10 +20,11 @@ class SimplePendulum extends ParticleSystem {
     for (let i = 0; i < this.maxParticles; i++) {
       const particle = this.particles[i];
       particle.x = particle.l * Math.sin(particle.angle);
-      particle.y = -particle.l * Math.cos(particle.angle) + 0.99;
-      particle.z = particle.l * (1 - Math.cos(particle.angle));
+      particle.y = -particle.l * Math.cos(particle.angle) + 150;
+      particle.z = particle.l * Math.cos(particle.angle2);
       const anularAcceleration =
         -(particle.g / particle.l) * Math.sin(particle.angle);
+
       particle.angleV += anularAcceleration;
       particle.angle += particle.angleV;
       this.geometry.attributes.position.setXYZ(
@@ -38,11 +41,11 @@ export const SimplePendulumAndConfig: SystemAndConfig = {
   name: "Simple Pendulum",
   system: new SimplePendulum(),
   config: {
-    particles: 500000,
-    zoom: 3,
+    particles: 200000,
+    zoom: 700,
     speed: 1,
-    sizeParticle: 0.003,
-    autoRotate: false,
+    sizeParticle: 1,
+    autoRotate: true,
     description: "",
   },
 };
