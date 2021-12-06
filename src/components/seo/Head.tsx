07dcb0c773a79f10b257b264c5e091f5ee78b59f
hernan castilla <hcastillaq@gmail.com>
2021-12-05 21:20:21 -0500

    change seo metas

--- src/components/seo/Head.tsx
+++ src/components/seo/Head.tsx
@@ -8,9 +8,21 @@ type Props = {
 const HeadSeo: React.FC<Props> = (props) => {
   return (
     <Head>
+      <meta name="google-site-verification" content="ElaVgP7AeT3eWe15NIfHZhMzSUEQqUsQFyUJ4y7TdxA" />
       <title>{props.title}</title>
       <meta name="description" content={props.description} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-VBL246FC32"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+          dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-VBL246FC32');
+      </script>
     </Head>
   );
 };
