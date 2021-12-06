de84cf33c519098c65a3b4954f53a5ef28cadf49
hernan castilla <hcastillaq@gmail.com>
2021-12-05 21:37:28 -0500

    fiex gtad

--- src/components/seo/Head.tsx
+++ src/components/seo/Head.tsx
@@ -5,24 +5,33 @@ type Props = {
   title: string;
   description: string;
 };
-const HeadSeo: React.FC<Props> = (props) => {
+const HeadSeo: React.FC<Props> = (props: Props) => {
   return (
     <Head>
-      <meta name="google-site-verification" content="ElaVgP7AeT3eWe15NIfHZhMzSUEQqUsQFyUJ4y7TdxA" />
+      <meta
+        name="google-site-verification"
+        content="ElaVgP7AeT3eWe15NIfHZhMzSUEQqUsQFyUJ4y7TdxA"
+      />
       <title>{props.title}</title>
       <meta name="description" content={props.description} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-VBL246FC32"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-VBL246FC32"
+      ></script>
 
-        function gtag() {
-          dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag() {
+            dataLayer.push(arguments);
+          }
+          gtag('js', new Date());
 
-        gtag('config', 'G-VBL246FC32');
-      </script>
+          gtag('config', 'G-VBL246FC32');`,
+        }}
+      />
     </Head>
   );
 };
