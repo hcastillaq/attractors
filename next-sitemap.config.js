02da726bfeb168f96679b1d6d9bfa266450763e3
hernan <hcastillaq@gmail.com>
2022-10-18 12:14:42 -0500

    delete pipeline

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://attractors-4ada9.web.app/",
+  siteUrl: process.env.SITE_URL || "https://g-art.vercel.app/",
   generateRobotsTxt: true,
 };
