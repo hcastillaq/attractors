c0ecb3868c9112eeccb46cf89108642e8032f3ee
hernan castilla <hcastillaq@gmail.com>
2023-03-05 14:32:46 -0500

    change api

--- src/helpers/fetch.helper.ts
+++ src/helpers/fetch.helper.ts
@@ -8,7 +8,6 @@ export const FETCH = async (url: string, config: FetchConfig) => {
 		method: config.method,
 		headers: {
 			"Content-Type": "application/json",
-			Authorization: `Bearer ${process.env.api_token}`,
 		},
 		body: config.body ? JSON.stringify(config.body) : null,
 	}).then((res) => res.json());
--- src/services/articles.service.ts
+++ src/services/articles.service.ts
@@ -1,18 +1,11 @@
 import { FETCH } from "../helpers/fetch.helper";
 
-const path = `${process.env.api}/gap-articles`;
+const path = `${process.env.api}/articles`;
 export const ArticlesService = {
 	getArticles: async () => {
-		return await FETCH(path, { method: "GET" })
-			.then((res) => res.data)
-			.then((res) => res.map((item: any) => item.attributes));
+		return await FETCH(path, { method: "GET" });
 	},
 	getArticleBySlug: async (slug: string) => {
-		return await FETCH(
-			`${path}?filters[slug][$eq]=${slug}&pagination[limit]=1`,
-			{ method: "GET" },
-		)
-			.then((res) => res.data)
-			.then((res) => (res[0] ? res[0].attributes : undefined));
+		return await FETCH(`${path}/${slug}`, { method: "GET" });
 	},
 };
